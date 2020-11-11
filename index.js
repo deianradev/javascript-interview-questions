5a0973deb369ab824c153b50805f69e23df99b5e
deianradev <deian.m.radev@gmail.com>
2020-11-11 18:44:43 -0500

    creating isPalindrome function to check if a word is a palindrome

--- index.js
+++ index.js
@@ -4,4 +4,27 @@ function log(data) {
   return console.log(data);
 }
 
-/* Please look at individual branches for any contents of this file */
+/* 
+Examples of Palindromes
+* racecar
+* nurses run
+* madam
+* eye
+* No lemon, no melon
+* Red rum, sir, is murder
+*/
+
+/* setup our string reverse function */
+function reverse(word) {
+  return [...word].reverse().join("");
+}
+
+function isPalindrome(word) {
+  //const re = /[^A-Za-z0-9]/g;
+  const re = /[\W]/g;
+  const lowercaseAndAlphaNumeric = word.replace(re, "").toLowerCase();
+  const reversedWord = reverse(lowercaseAndAlphaNumeric);
+  return lowercaseAndAlphaNumeric === reversedWord ? true : false;
+}
+
+log(isPalindrome("Red rum, sir, is murderzzzz"));
