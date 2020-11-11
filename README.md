6f8dc7db1ee69837e31c113c7496580ea7bb2dc2
deianradev <deian.m.radev@gmail.com>
2020-11-11 15:27:12 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# javascript-string-reverasal
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/javascript-string-reverasal)
---
+++ index.js
@@ -0,0 +1,58 @@
+console.clear();
+
+function log(data) {
+  return console.log(data);
+}
+
+//////////////////////////////////////////////////////
+/* using split, reverse and join */
+/*
+function reverse(word) {
+  return word
+    .split("")
+    .reverse()
+    .join("");
+}
+log(reverse('reverse'))
+*/
+
+//////////////////////////////////////////////////////
+/* using spread operator, reverse and join */
+/*
+function reverse(str) {
+  return [...str].reverse().join("");
+}
+log(reverse('reverse'))
+*/
+
+//////////////////////////////////////////////////////
+/*******  using for loop *******/
+/*
+function reverse(word) {
+  let temp = "";
+  for(let x = 0; x < word.length; x++) {
+    temp = word[x] + temp; 
+  }
+  return temp;
+}
+// 1st iteration temp is C: temp = word[0] + ""
+// 2nd iteration temp is AC: temp = word[1] + "c"
+// 3rd iteration temp is RAC: temp = word[2] + "ac"
+
+log(reverse("car"));
+*/
+
+//////////////////////////////////////////////////////
+/******  using for loop 2nd way ******/
+
+function reverse(word) {
+  let temp = "";
+  for (let x = word.length - 1; x >= 0; x--) {
+    //temp = temp + word[x];
+    temp = temp.concat(word[x]);
+  }
+  return temp;
+}
+//1st iteration E
+//2nd iteration S
+console.log(reverse("reverse"));
